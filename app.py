725b972b24d0731c93b9b3c12d604234658f2d7f
GeorgesML <georges.houmbadji@epitech.eu>
2023-02-08 01:59:11 +0100

    [UPD] Static rules reviewed and improved

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import datetime
 from pyspark.sql import SparkSession
+from pyspark.ml import PipelineModel
+from pyspark.sql.functions import col, when
 import streamlit as st
 from rules_static import get_odds
 
@@ -19,24 +22,41 @@ df_moves = read_csv_file(spark, "local_data/all_moves.csv")
 df_momo_moov = read_csv_file(spark, "local_data/momo_moov.csv")
 df_momo_mtn = read_csv_file(spark, "local_data/momo_mtn.csv")
 df_momo = df_momo_moov.union(df_momo_mtn)
+df_convictions = read_csv_file(spark, "local_data/convictions.csv")
 
 tooltip=['left:N', 'right:N', 'probe_id:N', 'diagram:N', 'count:N']
-add_selectbox = st.sidebar.markdown("<h1 style='text-align: center; color: black;'>OWLSIFY</h1>", unsafe_allow_html=True)
-col1, col2, col3 = st.columns(3)
+#add_selectbox = st.sidebar.markdown("<h1 style='text-align: center; color: black;'>OWLSIFY</h1>", unsafe_allow_html=True)
 
-with col1:
-    kind = st.selectbox('Search by', ['Nomads', 'Fraudster'])
-with col2:
-    start_date = str(st.date_input('From'))
-with col3:
-    end_date = str(st.date_input('To'))
+def static():
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        kind = st.selectbox('Search by', ['Nomads', 'Fraudster'])
+    with col2:
+        start_date = str(st.date_input('From', min_value=datetime.date(2000, 1, 1)))
+    with col3:
+        end_date = str(st.date_input('To', min_value=datetime.date(2000, 1, 1)))
+    with col4:
+        thres = int(st.number_input('Threshold', min_value=0, step=1))
+    # Response moves dataframe
+    df_mv = get_odds('moves', df_moves, 'Noms', start_date[:-3], end_date[:-3], thres)
+    # Response momo dataframe
+    df_mm = get_odds('fraudster', df_momo, 'relative_to', start_date[:-3], end_date[:-3], thres)
+    if kind == 'Nomads':
+        st.dataframe(df_mm, 800, 300)
+    elif kind == 'Fraudster':
+        st.dataframe(df_mv, 800, 300)
 
-# Response moves dataframe
-df_mv = get_odds('moves', df_moves, 'Noms', start_date[:-3], end_date[:-3], 2)
-# Response momo dataframe
-df_mm = get_odds('fraudster', df_momo, 'relative_to', start_date[:-3], end_date[:-3], 2)
+def dynamic():
+    savedPipelineModel = PipelineModel.load('rf-pipeline-model')
+    predictions = savedPipelineModel.transform(df_convictions)
+    cleanDF = predictions.select('Name', 'Age', 'prediction')
+    cleanDF = cleanDF.withColumn("level", when(cleanDF['prediction'] == 1, "suspect").otherwise('no suspect'))
+    st.dataframe(cleanDF, 100, 400)
 
-if kind == 'Nomads':
-    st.dataframe(df_mm, width=800, height=300)
-elif kind == 'Fraudster':
-    st.dataframe(df_mv, width=800, height=300)
+page_names_to_funcs = {
+    "STATIC": static,
+    "DYNAMIC": dynamic
+}
+
+demo_name = st.sidebar.selectbox("Inference Type", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
--- rules_static.py
+++ rules_static.py
@@ -1,4 +1,9 @@
-from pyspark.sql.functions import substring, when
+from pyspark.sql.functions import substring, when, col
+
+def get_diff(date_1, date_2):
+    year1, month1 = list(map(int, date_1.split('-')))
+    year2, month2 = list(map(int, date_2.split('-')))
+    return (year2-year1)*12 + month2-month1 + 1
 
 def get_odds(name, df, groupby, start_date, end_date, threshold=10):
     date_col = [col for col in df.columns if 'date' in col.lower()][0]
@@ -10,4 +15,13 @@ def get_odds(name, df, groupby, start_date, end_date, threshold=10):
                                      when(df_counts["count"] <= threshold, 0)
                                      .otherwise(1))
     df_counts = df_counts.drop("count")
-    return df_counts
+    combdf = (df_counts
+              .groupBy(groupby)
+              .sum()
+              .withColumnRenamed(f"sum({name}_odd)", "sum_odds"))
+    combdf = combdf.withColumn("ratio", col("sum_odds")/get_diff(start_date, end_date))
+    combdf = combdf.withColumn("level",
+                               when((combdf["ratio"] >= 0) & (combdf["ratio"] <= 0.3), 'low')
+                               .when((combdf["ratio"] > 0.3) & (combdf["ratio"] <= 0.6), 'medium')
+                               .when((combdf["ratio"] > 0.6), 'high'))
+    return combdf
